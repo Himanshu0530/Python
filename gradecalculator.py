b33f6c6d711505e30fa15642b0f092fed9739a88
Himanshu0530 <pandeyhimanshushekhar568@gmail.com>
2026-07-07 17:29:38 +0530

    added grade calculator

---
+++ gradecalculator.py
@@ -0,0 +1,10 @@
+#grade calculator
+Marks = int(input("Marks "))
+if(Marks >= 90 ):
+    print("A+")
+elif(Marks >= 80 and Marks < 90 ):
+    print("A")
+elif(Marks >= 70 and Marks >= 80 ):
+    print("B+")
+else : 
+    print("C")
